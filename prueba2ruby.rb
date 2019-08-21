1a48da774e1c166cc9236fc3fdb7c6b17026f058
MirsonCamadro <kztempus@gmail.com>
2019-08-21 10:58:20 -0400

    added line brakes and new functionality, option 1: average

--- prueba2ruby.rb
+++ prueba2ruby.rb
@@ -12,24 +12,51 @@ while option != 4
   puts '
   Bienvenido, ingrese una opcion:
 
-  1) Alumnos y sus promedios
+    1) Alumnos y sus promedios
 
-  2) Alumnos y sus inacistencias
+    2) Alumnos y sus inacistencias
 
-  3) Alumnos aprobados
+    3) Alumnos aprobados
 
-  4) Salir'
+    4) Salir'
 
 option = gets.chomp.to_i
 
+def line_break
+  puts '--------------------------------------------------------------'
+end
+
+def average
+  grades_list = read_alum('gradestable.csv')
+  grades_list.each do |total_grades|
+    sum_grades = 0
+    count_grades = total_grades.size - 1
+      total_grades.each_with_index do |grades, index|
+        sum_grades += grades.to_i if index != 0
+      end
+    average = sum_grades.to_f / count_grades
+    puts "El alumno #{total_grades[0]} obtuvo un promedio de #{average}"
+  end
+end
+
+
+
+
+
   case option
   when 1
+    average
+    line_break
   when 2
+    line_break
   when 3
+    line_break
   when 4
     puts 'Hasta pronto, have a nice day (ᵔᴥᵔ) '
+    line_break
   else
     puts 'Opcion no valida, porfavor ingrese opcion del 1-4'
+    line_break
   end
 
 
